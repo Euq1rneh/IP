6d99fbf8ea821e5efcbb19b84e92e6e5a647648b
Henrique Marques <93006999+Euq1rneh@users.noreply.github.com>
2021-11-11 12:30:38 +0000

    Add files via upload

--- TP6/ISBNGenerator.java
+++ TP6/ISBNGenerator.java
@@ -6,6 +6,9 @@ public class ISBNGenerator {
         */
         System.out.println(isbnGenerator(020131452));// inteiro ontroduzido e completamente diferente do que e utilizado mas nao excede a capacidade de representacao do int
         System.out.println(isbnGenerator(972662905)); // funciona ultimo digito = 5
+        System.out.println(isbnGenerator(999215810));// ultimo digito 7
+        System.out.println(isbnGenerator(997150210));//ultimo digito 0
+        System.out.println(isbnGenerator(853590277));//ultimo digito 5
     }
 
     private static String isbnGenerator(int inteiro){ 
